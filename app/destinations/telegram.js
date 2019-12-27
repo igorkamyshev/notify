f3d5c1a527b22af500aacff84d916cfb455caba2
Igor Kamyshev <igor@kamyshev.me>
2019-12-27 12:10:40 +0300

    fix: change telegram message format

--- app/destinations/telegram.js
+++ app/destinations/telegram.js
@@ -1,21 +1,27 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TELEGRAM_KEY
+const token = process.env.TELEGRAM_KEY;
 
-const bot = new TelegramBot(token, { polling: true })
+const bot = new TelegramBot(token, { polling: true });
 
-const send = (user, message) => bot.sendMessage(user, message, { parse_mode: 'Markdown' })
+const send = (user, message) =>
+  bot.sendMessage(user, message, { parse_mode: 'Markdown' });
 
-bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id
+bot.onText(/\/start/, async msg => {
+  const chatId = msg.chat.id;
 
-  await send(chatId, 'Hello! For API requests use following information.')
-  await send(chatId, `\`user: "${chatId}"\``)
-})
+  await send(chatId, 'Hello! For API requests use following information.');
+  await send(chatId, `\`user: "${chatId}"\``);
+});
 
-const createMessage = data => ['----', `*Medium:* ${data.medium}`, `*From*: ${data.name} <${data.from}>`, data.text].join('\n\n')
-const sendMessage = (user, data) => send(user, createMessage(data))
+const createMessage = data =>
+  [
+    `*From*: ${data.name} <${data.from}>`,
+    data.text,
+    `medium: ${data.medium}`,
+  ].join('\n\n');
+const sendMessage = (user, data) => send(user, createMessage(data));
 
 module.exports = {
   sendMessage,
-}
+};
